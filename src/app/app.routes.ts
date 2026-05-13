5a50ad15b6c55ccb73492aca935b7517bb479efa
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2026-05-13 10:47:00 +0200

    feat(init): Add tailwindcss, add homepage, header, footer and login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/login/login';
+import { Homepage } from './features/homepage/homepage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Homepage,
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+];
